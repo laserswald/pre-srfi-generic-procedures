ab920e135a6b0938f621c4351a4ecd3a51a40a6d
Ben Davenport-Ray <ben.davenportray@gmail.com>
2021-07-28 11:24:30 -0400

    Delete roles-scratch.scm

--- roles-scratch.scm
+++
@@ -1,9 +0,0 @@
-
-(define-typeclass <functor>
-  (fmap proc fctr))
-
-(define-instance (<functor> <list> fmap) map)
-(define-instance (<functor> <vector> fmap) vector-map)
-
-(define-typeclass (<applicative> <functor>))
-  
